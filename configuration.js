2c2e69086c0310e0fcc44f70e50974f14103a6ff
janca777 <shopping.alias@ymail.com>
2020-03-27 13:54:32 +0100

    Update configuration.js to run in Jenkins

--- configuration.js
+++ configuration.js
@@ -1,16 +1,25 @@
 var Jasmine2HtmlReporter = require("protractor-jasmine2-html-reporter");
 
 exports.config = {
-   capabilities: {
-      browserName: 'chrome',
-      //browserName: 'firefox',
-      marionette: true
-   },
-   seleniumAddress: 'http://localhost:4444/wd/hub',
-   directConnect: true,
+
+   /*
+      capabilities: {
+         browserName: 'chrome',
+         //browserName: 'firefox',
+         // marionette: true
+      },
+      seleniumAddress: 'http://localhost:4444/wd/hub',
+      directConnect: true,
+    */
+     
    specs: ['spec.google.buchner.js'],
+   allScriptsTimeout: 50000,
+   getPageTimeout: 50000,
+   framework: 'jasmine2',
+
+
    onPrepare: () => {
-      browser.driver.manage().window().maximize();
+      //browser.driver.manage().window().maximize();
       jasmine.getEnv().addReporter(
          new Jasmine2HtmlReporter({
             savePath: 'target/screenshots'
@@ -18,8 +27,8 @@ exports.config = {
       );//addReporter
    },//onPrepare
    jasmineNodeOpts: {
-      showColors: true // Use colors in the command line report.
+      showColors: true, // Use colors in the command line report.
+      defaultTimeoutInterval: 50000
    }
 
 };
-
